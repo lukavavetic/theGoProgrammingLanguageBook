4b43c6a89c13457632bcaf983f87002b5fcc0010
Luka Vavetić <luka.vavetic@gmail.com>
2020-01-17 20:53:35 +0100

    2.3. Pointers

---
+++ ProgramStructure/pointers.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 1
+	p := &x         // p, of type *int, points to x
+	fmt.Println(*p) // "1"
+	*p = 2          // equivalent to x = 2
+	fmt.Println(x)  // "2"
+
+	name := "Luka"
+
+	newName := &name
+
+	fmt.Println(name) //Luka
+	fmt.Println(newName) //print address of variable name
+	fmt.Println(*newName) //print value of variable name - Luka
+
+	*newName = "Matija"
+
+	fmt.Println(name) // Matija
+	fmt.Println(*newName) // Matija
+}
