648a242a8b5326f9d84ffa0a338508769c531e50
Luka Vavetić <luka.vavetic@gmail.com>
2020-01-19 16:03:54 +0100

    3.6. Strings

---
+++ ProgramStructure/strings.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	//s := "hello, world"
+	//fmt.Println(len(s))     // "12"
+	//fmt.Println(s[0], s[7]) // "104 119"  ('h' and 'w')
+	//
+	//fmt.Println(s[0:5]) // "hello"
+	//
+	//
+	//fmt.Println(s[:5]) // "hello"
+	//fmt.Println(s[7:]) // "world"
+	//fmt.Println(s[:])  // "hello, world"
+
+
+	//s := "left foot"
+	////s := "right foot" // error : no new variables on left side of :=
+	//t := s
+	//s += ", right foot"
+	//
+	//fmt.Println(s) // "left foot, right foot"
+	//fmt.Println(t) // "left foot"
+
+	//s[0] = 'L' // compile error: cannot assign to s[0]
+}
